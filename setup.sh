ba2b1be0fc89895803e89dd454d37e39794d39a1
Juniyasyos <juniyasyos@gmail.com>
2024-10-16 22:55:15 +0700

    fix: config

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ apt update && apt install -y curl apache2
 TOKEN=$(curl -s --request PUT "http://169.254.169.254/latest/api/token" --header "X-aws-ec2-metadata-token-ttl-seconds: 3600")
 ip=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4 --header "X-aws-ec2-metadata-token: $TOKEN")
 
-repo_url="https://github.com/juniyasyos/shell_scripting.git"
+repo_url="https://github.com/Agung-67/shell_scripting.git"
 repo_dir="/var/www/html/porto"
 
 git clone "$repo_url" "$repo_dir"
@@ -25,3 +25,6 @@ mkdir -p /var/www/html/pages/{shafa,agung,dahlan}
 git clone https://github.com/juniyasyos/portofolio.git /var/www/html/pages/dahlan/porto
 git clone https://github.com/Agung-67/cv2.git /var/www/html/pages/agung/porto
 git clone https://github.com/shafaybkr23/Website-Portofolio.git /var/www/html/pages/shafa/porto
+git clone https://github.com/maniknur/myCV.git /var/www/html/pages/manik/porto
+# Mbak Ima 
+# Mas azzuri
